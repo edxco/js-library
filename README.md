45cfdf6a6e94c46b493a419883711b1770a9f592
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-02-01 16:06:49 -0600

    Readme add & ESlint error fix

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# js-library
+# BooksLibrary-JS-Practice
+
+> In this project we built a a simple app where you are able to load the books you have read, with their description, pages and read status. 
+
+## Built With
+
+- HTML
+- CSS
+- Vanilla JavaScript
+- Firebase realtime database
+
+## Live Demo
+[Live Demo](https://edxco.github.io/js-library/)
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+# Instructions (Set Up)
+
+Clone this repository in your computer
+```
+$  git clone https://github.com/edxco/js-library.git
+```
+Access the folder where you cloned the project in your explorer and open index.html
+
+## Authors
+
+👤 **Eduardo Baeza**
+
+- GitHub: [@edxco](https://github.com/edxco/)
+- Twitter: [@lalo_nbc](https://twitter.com/lalo_nbc/)
+- LinkedIn: [eduardo-n-baeza](https://www.linkedin.com/in/eduardo-n-baeza/)
+
+👤 **Kingsley Ibeh**
+
+- GitHub: [@githubhandle](https://github.com/Kingobaino1)
+- Twitter: [@twitterhandle](https://twitter.com/ibehkingso)
+- Linkedin: [linkedin] (https://www.linkedin.com/in/ibeh-kingsley-obinna-568596177)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+Feel free to check the issues page.
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- [Microverse](https://www.microverse.org/).
+- [The Odin Project](https://www.theodinproject.com/).
+
+## 📝 License
+
+This project is [MIT licensed](https://github.com/chubaquelo/re-former/blob/form/LICENSE).
--- js/library.js
+++ js/library.js
@@ -1,136 +1,127 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyAkEY49W_RKvLI6rcO_r6SjnrJUvwduu2U",
-  authDomain: "library-836fa.firebaseapp.com",
-  databaseURL: "https://library-836fa-default-rtdb.firebaseio.com",
-  projectId: "library-836fa",
-  storageBucket: "library-836fa.appspot.com",
-  messagingSenderId: "742428988728",
-  appId: "1:742428988728:web:9081a66a88d2169030ddcd",
-  measurementId: "G-7T30W83E97"
+/* global firebase */
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAkEY49W_RKvLI6rcO_r6SjnrJUvwduu2U',
+  authDomain: 'library-836fa.firebaseapp.com',
+  databaseURL: 'https://library-836fa-default-rtdb.firebaseio.com',
+  projectId: 'library-836fa',
+  storageBucket: 'library-836fa.appspot.com',
+  messagingSenderId: '742428988728',
+  appId: '1:742428988728:web:9081a66a88d2169030ddcd',
+  measurementId: 'G-7T30W83E97',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 function Book(title, author, page, read) {
-  this.title = title
-  this.author = author
-  this.page = page
-  this.read = read
-}
-
-function error(error) {
-  if (error) {
-    alert('Houston! we have a problem!')
-  } else {
-    alert('Data saved successfully!')
-  }
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read;
 }
 
 function displayBooksSaved() {
-  firebase.database().ref('books').orderByChild('author').on('value', function (snapshot) {
-
-    snapshot.forEach(function (childSnapshot) {
-      var childData = childSnapshot.val();
-
-      let key = childSnapshot.key
-      let b_title = childSnapshot.val().title;
-      let b_author = childSnapshot.val().author;
-      let b_page = childSnapshot.val().page;
-      let b_read = childSnapshot.val().read;
+  firebase.database().ref('books').orderByChild('title').on('value', (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const { key } = childSnapshot;
+      const dbTitle = childSnapshot.val().title;
+      const dbAuthor = childSnapshot.val().author;
+      const dbPage = childSnapshot.val().page;
+      const dbRead = childSnapshot.val().read;
 
       const u = document.querySelector('#container-card');
 
-      let ul = document.createElement('div');
-      let a = document.createElement('div');
-      let b = document.createElement('div');
-      let c = document.createElement('div');
-      let d = document.createElement('div');
-      const li = document.createElement('div');
+      const ul = document.createElement('div');
 
-      a.innerHTML = `<strong class="fs-3 fw-light">${b_title}</strong>`;
-      b.innerHTML = `<strong>Author:</strong> ${b_author}`;
-      c.innerHTML = `<strong>Pages:</strong> ${b_page}`;
+      const a = document.createElement('div');
+      a.innerHTML = `<span class="fs-3 fw-light">${dbTitle}</span>`;
 
-      let status = 'Already read'
-      if (b_read) {
-        status = 'Yet to read'
+      const b = document.createElement('div');
+      b.classList.add('list-group-item');
+      b.innerHTML = `<strong>Author:</strong> ${dbAuthor}`;
+
+      const c = document.createElement('div');
+      c.classList.add('list-group-item');
+      c.innerHTML = `<strong>Pages:</strong> ${dbPage}`;
+
+      const d = document.createElement('div');
+      d.classList.add('list-group-item');
+      let status = 'Already read';
+      let statusButton = 'Mark as unread';
+      if (!dbRead) {
+        status = 'Yet to read';
+        statusButton = 'Mark as read';
       }
-
       d.innerHTML = `${status}`;
 
-      let button1 = document.createElement('button')
-      button1.innerHTML = 'Erase'
-      button1.setAttribute('id', 'erase');
-      button1.classList.add('btn', 'btn-primary')
-
-      let button2 = document.createElement('button')
-      button2.innerHTML = `${status}`
-      button2.setAttribute('id', 'change');
-      button2.classList.add('btn', 'btn-primary')
-
-      u.appendChild(ul)
-      ul.classList.add('mx-auto', 'my-3');
-      ul.appendChild(a).classList.add('card', 'text-dark', 'bg-light', 'p-3');
-      ul.appendChild(b).classList.add("list-group-item");
-      ul.appendChild(c).classList.add("list-group-item");
-      ul.appendChild(d).classList.add("list-group-item");
-      ul.appendChild(button1);
+      const button1 = document.createElement('button');
+      button1.innerHTML = 'Delete';
+      button1.classList.add('btn', 'btn-danger', 'mr-1', 'mt-2', 'mb-4');
       button1.setAttribute('data-pos', key);
-      ul.appendChild(button2);
+
+      const button2 = document.createElement('button');
+      button2.innerHTML = `${statusButton}`;
+      button2.classList.add('btn', 'btn-info', 'mx-1', 'mt-2', 'mb-4');
       button2.setAttribute('data-pos', key);
 
-      button2.addEventListener('click', (e) => {
-        let route = e.target.getAttribute('data-pos');
-        let db_firebase = firebase.database().ref('books/' + route);
+      ul.classList.add('mx-auto', 'my-3');
+      ul.appendChild(a).classList.add('card', 'text-dark', 'bg-light', 'p-3');
+      ul.appendChild(b);
+      ul.appendChild(c);
+      ul.appendChild(d);
+      ul.appendChild(button1);
+      ul.appendChild(button2);
 
-        console.log(b_read)
-        if (b_read) {
-          db_firebase.update({
-            'read': false
-          })
+      button2.addEventListener('click', (e) => {
+        const route = e.target.getAttribute('data-pos');
+        const dbFirebase = firebase.database().ref(`books/${route}`);
+
+        if (dbRead) {
+          dbFirebase.update({
+            read: false,
+          });
         } else {
-          db_firebase.update({
-            'read': true
-          })
+          dbFirebase.update({
+            read: true,
+          });
         }
-      })
+        window.location.reload();
+      });
 
       button1.addEventListener('click', (e) => {
-        let route = e.target.getAttribute('data-pos');
-        let db_firebase = firebase.database().ref('books/' + route);
+        const route = e.target.getAttribute('data-pos');
+        const dbFirebase = firebase.database().ref(`books/${route}`);
 
-        db_firebase.remove()
+        dbFirebase.remove();
+        window.location.reload();
+      });
 
-      })
+      u.appendChild(ul);
     });
   });
 }
 
 function addBookToLibrary() {
-  title = document.getElementById('title').value;
-  author = document.getElementById('author').value;
-  page = document.getElementById('page').value;
-  read = document.getElementById('read').checked;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const page = document.getElementById('page').value;
+  const read = document.getElementById('read').checked;
 
-  let book = new Book(title, author, page, read)
+  const book = new Book(title, author, page, read);
 
   firebase.database().ref('books').push(book);
-  //set(push(book), error(error)); 
 }
-
-let but_addBook = document.querySelector('#btn')
-but_addBook.addEventListener('click', appear)
 
 function appear() {
-  document.getElementById('addBook').className = "d-block";
+  document.getElementById('addBook').className = 'd-block';
 }
 
-let submit = document.querySelector('.submit')
-submit.addEventListener('click', addBookToLibrary)
+const buttonAddBook = document.querySelector('#btn');
+buttonAddBook.addEventListener('click', appear);
 
-let display = document.querySelector('#ex-table')
+const submit = document.querySelector('.submit');
+submit.addEventListener('click', addBookToLibrary);
 
-displayBooksSaved()
+displayBooksSaved();
