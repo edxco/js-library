3beeb0bd369f11e1944cc6b36a673bf4a86a8bc6
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-02-02 11:39:43 -0600

    Error form validation display

--- js/library.js
+++ js/library.js
@@ -22,6 +22,75 @@ function Book(title, author, page, read) {
   this.read = read;
 }
 
+function elements(key, dbTitle, dbAuthor, dbPage, dbRead) {
+  const u = document.querySelector('#container-card');
+
+  const ul = document.createElement('div');
+
+  const a = document.createElement('div');
+  a.innerHTML = `<span class="fs-3 fw-light">${dbTitle}</span>`;
+
+  const b = document.createElement('div');
+  b.classList.add('list-group-item');
+  b.innerHTML = `<strong>Author:</strong> ${dbAuthor}`;
+
+  const c = document.createElement('div');
+  c.classList.add('list-group-item');
+  c.innerHTML = `<strong>Pages:</strong> ${dbPage}`;
+
+  const d = document.createElement('div');
+  d.classList.add('list-group-item');
+  let status = 'Already read';
+  let statusButton = 'Mark as unread';
+  if (!dbRead) {
+    status = 'Yet to read';
+    statusButton = 'Mark as read';
+  }
+  d.innerHTML = `${status}`;
+
+  const button1 = document.createElement('button');
+  button1.innerHTML = 'Delete';
+  button1.classList.add('btn', 'btn-danger', 'mr-1', 'mt-2', 'mb-4');
+  button1.setAttribute('data-pos', key);
+
+  const button2 = document.createElement('button');
+  button2.innerHTML = `${statusButton}`;
+  button2.classList.add('btn', 'btn-info', 'mx-1', 'mt-2', 'mb-4');
+  button2.setAttribute('data-pos', key);
+
+  ul.classList.add('mx-auto', 'my-3');
+  ul.appendChild(a).classList.add('card', 'text-dark', 'bg-light', 'p-3');
+  ul.appendChild(b);
+  ul.appendChild(c);
+  ul.appendChild(d);
+  ul.appendChild(button1);
+  ul.appendChild(button2);
+
+  button2.addEventListener('click', (e) => {
+    const route = e.target.getAttribute('data-pos');
+    const dbFirebase = firebase.database().ref(`books/${route}`);
+
+    if (dbRead) {
+      dbFirebase.update({
+        read: false,
+      });
+    } else {
+      dbFirebase.update({
+        read: true,
+      });
+    }
+  });
+
+  button1.addEventListener('click', (e) => {
+    const route = e.target.getAttribute('data-pos');
+    const dbFirebase = firebase.database().ref(`books/${route}`);
+
+    dbFirebase.remove();
+  });
+
+  u.appendChild(ul);
+}
+
 function displayBooksSaved() {
   firebase.database().ref('books').orderByChild('title').on('value', (snapshot) => {
     snapshot.forEach((childSnapshot) => {
@@ -37,16 +106,16 @@ function displayBooksSaved() {
 }
 
 function addBookToLibrary() {
-  
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const page = document.getElementById('page').value;
   const read = document.getElementById('read').checked;
 
-  if (title == "" || author == "" || page == "") {
-    alert('Some inputs are empty')
-  }
-  else {
+  if (title === '' || author === '' || page === '') {
+    const warningField = document.getElementById('txtWarning');
+    warningField.innerHTML = '<p class="fs-6">Some fields are empty</p>';
+    warningField.appendChild(warningField);
+  } else {
     const book = new Book(title, author, page, read);
     firebase.database().ref('books').push(book);
   }
